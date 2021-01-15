cab44b51de202789ddc2c855edff5afb77908406
Jonathan Tuzman <jtuzman@sunriselabs.com>
2021-01-15 11:36:26 -0500

    work on casting instructions

--- HackAssemblerTests/ParserTest.cpp
+++ HackAssemblerTests/ParserTest.cpp
@@ -12,23 +12,28 @@ TEST(ParserA, parsesAInstructionType)
 	AssemblyParser p("@1", 0);
 	EXPECT_EQ(p.getInstruction().getInstructionType(), AssemblerInstruction::InstructionType::A);
 }
-/* 
 TEST(ParserA, parsesBuiltInConstants)
 {
-	Parser p("@R3", 0);
-	EXPECT_EQ(static_cast<AInstruction>(p.getInstruction()).getLine(), "R3");
+	AssemblerInstruction inst = Parser("@R3", 0).getInstruction();
+	EXPECT_EQ(inst.getInstructionType(), AssemblerInstruction::InstructionType::A);
+	AssemblerInstruction* i = &(inst);
+	AInstruction* a = dynamic_cast<AInstruction*>(i);
+	std::string line = a->getLine();
+
+	EXPECT_EQ(a->getLine(), "R3");
 }
+/*
 
 TEST(ParserA, parsesNumericConstant)
 {
 	Parser p("@15", 0);
-	EXPECT_EQ(static_cast<AInstruction>(p.getInstruction()).getLine(), "15");
+	EXPECT_EQ(dynamic_cast<AInstruction>(p.getInstruction()).getLine(), "15");
 }
 
 TEST(ParserA, parsesVariable)
 {
 	Parser p("@abc", 0);
-	EXPECT_EQ(static_cast<AInstruction>(p.getInstruction()).getLine(), "abc");
+	EXPECT_EQ(dynamic_cast<AInstruction>(p.getInstruction()).getLine(), "abc");
 }
  */
 TEST(ParserC, parsesCInstructionTypeFull)
