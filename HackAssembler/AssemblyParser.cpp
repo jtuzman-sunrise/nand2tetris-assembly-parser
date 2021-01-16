b6586e448fee130cbec0c8e26948b753d081fd9f
Jonathan Tuzman <jtuzman@sunriselabs.com>
2021-01-15 20:49:59 -0500

    Fix C instruction parsing

--- HackAssembler/AssemblyParser.cpp
+++ HackAssembler/AssemblyParser.cpp
@@ -11,39 +11,47 @@ using namespace std;
 
 AssemblyParser::AssemblyParser(string assemblyCode, unsigned int lineNumber)
 {
-    // non-instruction lines should be handled BEFORE we get to the parser
-    if (assemblyCode.find("(") == 0 
-    || assemblyCode.find("//") != string::npos  
-    ||  assemblyCode.find(" ") != string::npos) {
-        throw invalid_argument("Only actual instructions, without spaces, should be fed to the Parser.");
-    }
-    
-    // a-instruction
-    if (assemblyCode.find("@") == 0) {
-        mInstruction = AInstruction(assemblyCode.substr(1), lineNumber);
-        mInstructionPtr = new AInstruction(assemblyCode.substr(1), lineNumber);
+	// non-instruction lines should be handled BEFORE we get to the parser
+	if (assemblyCode.find("(") == 0
+		|| assemblyCode.find("//") != string::npos
+		|| assemblyCode.find(" ") != string::npos) {
+		throw invalid_argument("Only actual instructions, without spaces, should be fed to the Parser.");
+	}
 
-        mUniqueInstructionPtr = make_unique<AInstruction>(assemblyCode.substr(1), lineNumber);
-        return;
-    } 
+	// a-instruction
+	if (assemblyCode.find("@") == 0) {
+		mInstruction = AInstruction(assemblyCode.substr(1), lineNumber);
+		mInstructionPtr = new AInstruction(assemblyCode.substr(1), lineNumber);
 
-    // c-instruction
-    smatch matches;
-    auto regex_str = regex("([AMD]{1,3}=)?([01\-AMD!|+&><]{1,3})?(;[A-Z]{3})?");
-    regex_search(assemblyCode, matches, regex_str);
-    string dest = matches[1], comp = matches[2], jump = matches[3];
+		mUniqueInstructionPtr = make_unique<AInstruction>(assemblyCode.substr(1), lineNumber);
+		return;
+	}
 
-    mInstructionPtr = new CInstruction(comp.substr(0, comp.length() - 1), dest, jump.substr(1);
+	// c-instruction
+	smatch matches;
+	auto regex_str = regex("([AMD]{1,3}=)?([01\\-AMD!|+&><]{1,3})?(;[A-Z]{3})?");
+	regex_search(assemblyCode, matches, regex_str);
+	string dest = matches[1]; 
+	string comp = matches[2]; 
+	string jump = matches[3];
+
+	auto d1 = matches[2].first;
+	auto d2 = matches[2].second;
+	auto st = string(d1,d2);
+
+	mInstructionPtr = new CInstruction(comp,
+		dest.empty() ? "" : dest.substr(0, dest.length() - 1),
+		jump.empty() ? "" : jump.substr(1));
 }
 
 unique_ptr<AssemblerInstruction> AssemblyParser::getUniqueInstructionPtr()
 {
-    return make_unique<AssemblerInstruction>(mInstruction);
-    //return mUniqueInstructionPtr;
+	return make_unique<AssemblerInstruction>(mInstruction);
+	//return mUniqueInstructionPtr;
 }
 
 AssemblerInstruction* AssemblyParser::getInstructionPtr()
 {
-    //return &mInstruction;
-    return mInstructionPtr;
+	//return &mInstruction;
+	return mInstructionPtr;
 }
--- HackAssemblerTests/ParserCTest.cpp
+++ HackAssemblerTests/ParserCTest.cpp
@@ -9,36 +9,36 @@ using Parser = AssemblyParser;
 TEST(ParserC, parsesCInstructionTypeFull)
 {
 	// parse an a C and test its instruction type
-	auto ptr = Parser("A=M;JMP", 0).getInstructionPtr();
+	auto ptr = Parser("A=D-1;JMP", 0).getInstructionPtr();
 	EXPECT_EQ(ptr->getInstructionType(), AssemblerInstruction::InstructionType::C);
 
 	CInstruction* c = dynamic_cast<CInstruction*>(ptr);
 	EXPECT_EQ(c->getDest(), "A");
-	EXPECT_EQ(c->getComp(), "M");
+	EXPECT_EQ(c->getComp(), "D-1");
 	EXPECT_EQ(c->getJump(), "JMP");
 }
 
 TEST(ParserC, parsesCInstructionTypeNoJump)
 {
 	// parse an a C and test its instruction type
-	auto ptr = Parser("D=D-M", 0).getInstructionPtr();
+	auto ptr = Parser("D=D+M", 0).getInstructionPtr();
 	EXPECT_EQ(ptr->getInstructionType(), AssemblerInstruction::InstructionType::C);
 
 	CInstruction* c = dynamic_cast<CInstruction*>(ptr);
 	EXPECT_EQ(c->getDest(), "D");
-	EXPECT_EQ(c->getComp(), "D-M");
+	EXPECT_EQ(c->getComp(), "D+M");
 	EXPECT_EQ(c->getJump(), "");
 }
 
 TEST(ParserC, parsesCInstructionTypeNoDest)
 {
 	// parse an a C and test its instruction type
-	auto ptr = Parser("M;JMP", 0).getInstructionPtr();
+	auto ptr = Parser("A&M;JMP", 0).getInstructionPtr();
 	EXPECT_EQ(ptr->getInstructionType(), AssemblerInstruction::InstructionType::C);
 
 	CInstruction* c = dynamic_cast<CInstruction*>(ptr);
-	EXPECT_EQ(c->getDest(), "M");
-	EXPECT_EQ(c->getComp(), "");
+	EXPECT_EQ(c->getDest(), "");
+	EXPECT_EQ(c->getComp(), "A&M");
 	EXPECT_EQ(c->getJump(), "JMP");
 }
 
